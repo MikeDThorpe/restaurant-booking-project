1556328aa15f2010998b03a0fb83b872ee8e3097
levilu800b <levilu800b@gmail.com>
2021-02-16 19:26:51 +0000

    select

--- src/components/Form.js
+++ src/components/Form.js
@@ -86,6 +86,14 @@ class Form extends React.Component {
                   name="numberofguests"
                   placeholder="Choose Your Option">
                   <option value="">Please select</option>
+                  <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
                 </select>
               </Col>
             </Row>
