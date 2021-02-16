aa3a8a47f64b213ef1ea5d8f04aae7a39b98c34e
TTBoiMike <michaelthorpe747@hotmail.com>
2021-02-16 20:47:43 +0000

    create slider component with styling

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as RestaurantCards } from './RestaurantCards';
+export { default as ImageCarousel } from './ImageCarousel';
 export { default as Footer } from "./Footer";
 export { default as Navbar } from "./Navbar";
