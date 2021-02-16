c1e430643e002ffb78ad48db9b06ab80e78e4a3e
levilu800b <levilu800b@gmail.com>
2021-02-16 12:24:22 +0000

    booking

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
-import './styles/App.css';
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
-import { Home, Menu, Restaurants } from "./pages";
+import React, { Component } from 'react';
+import './App.css';
+import Form from './components/Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/menu" component={Menu} />
-          <Route path="/restaurants" component={Restaurants} />
-          {/* Setup dymanic routing for restaurant pages  */}
-          {/* <Route path="/restaurants">
-            <Route path="/antarctic" component={Restaurants} />
-            <Route path="/arctic" component={Restaurants} />
-            <Route path="/sahara" component={Restaurants} />
-            <Route path="/arabian" component={Restaurants} />
-            <Route path="/gobi" component={Restaurants} />
-          </Route>*/}
-        </Switch> 
-      </Router>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+        <div className="App">
+         <Form />
+        </div>
+    )
+}
 }
 
-export default App;
+
+export default App
