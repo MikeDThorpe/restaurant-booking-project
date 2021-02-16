d4f83beb7de4b14bfd26e71eec6685d9609bf197
Levi Udeh <72051406+levilu800b@users.noreply.github.com>
2021-02-16 09:40:58 +0000

    Revert "form"

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import React, { Component } from 'react';
-import './App.css';
-import Form from './components/Form';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/App.css';
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { Home, Menu, Restaurants } from "./pages";
 
-class App extends Component {
-  render() {
-    return (
-        <div className="App">
-         <Form />
-        </div>
-    )
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/menu" component={Menu} />
+          <Route path="/restaurants" component={Restaurants} />
+          {/* Setup dymanic routing for restaurant pages  */}
+          {/* <Route path="/restaurants">
+            <Route path="/antarctic" component={Restaurants} />
+            <Route path="/arctic" component={Restaurants} />
+            <Route path="/sahara" component={Restaurants} />
+            <Route path="/arabian" component={Restaurants} />
+            <Route path="/gobi" component={Restaurants} />
+          </Route>*/}
+        </Switch> 
+      </Router>
+    </div>
+  );
 }
-}
 
-
-export default App
+export default App;
