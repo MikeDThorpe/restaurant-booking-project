732f34f359cecc079929b9cbdd49d1072b9a9dbf
matthewcotton <matthewneilcotton@gmail.com>
2021-02-18 16:49:00 +0000

    tidy up App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './styles/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { HashRouter as Router, Switch, Route, useParams } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { Home, Menu, Restaurants } from "./pages";
 import { Navbar, Footer } from "./components";
 import Form from "./components/Form"
@@ -14,14 +14,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/menu" component={Menu} />
-          {/* <Route path="/restaurants" component={Restaurants} /> */}
-          {/* Setup dymanic routing for restaurant pages  */}
           <Route path="/restaurants/:name" children={<Restaurants />}>
-            {/* <Route path="/antarctic" component={Restaurants} />
-            <Route path="/arctic" component={Restaurants} />
-            <Route path="/sahara" component={Restaurants} />
-            <Route path="/arabian" component={Restaurants} />
-            <Route path="/gobi" component={Restaurants} /> */}
           </Route>
         </Switch> 
         <Form />
